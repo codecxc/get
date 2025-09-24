a89369d1164ad8a93a186f6363c427042b5c314a
Your Name <codecxc@github.com>
2025-09-24 16:29:37 +0300

    get

--- auto-light.py
+++ auto-light.py
@@ -8,7 +8,7 @@ led=26
 GPIO.setup(photo,GPIO.IN)
 GPIO.setup(led,GPIO.OUT)
 #state=0
-#period=0.2
+#period=0.2                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
 a=True
 while a:
     res=GPIO.input(photo)
--- double-push.py
+++ double-push.py
@@ -4,6 +4,7 @@ import time
 def dec2bin(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
 
+
 GPIO.setmode(GPIO.BCM)
 leds=[16,12,25,17,27,23,22,24]
 GPIO.setup(leds,GPIO.OUT)
@@ -12,8 +13,20 @@ buttons=[9,10]
 GPIO.setup(buttons,GPIO.IN)
 num=0
 sleep_time=0.2
-while True:
+double_pres=0.5
+def check():
     if GPIO.input(buttons[0]):
+        time.sleep(0.1)
+        if GPIO.input(buttons[1]):
+            return True
+    elif GPIO.input(buttons[1]):
+        time.sleep(0.1)
+        if GPIO.input(buttons[0]):
+            return True
+while True:
+    if check():
+        num=255
+    elif GPIO.input(buttons[0]):
         if(num==255): continue
         else:
             num+=1
@@ -22,7 +35,9 @@ while True:
     elif GPIO.input(buttons[1]):
         if(num==0): continue
         else:
-            num-=1
-            print(num,dec2bin(num))
-            time.sleep(sleep_time)
+            if(num==255): num=0
+            else:
+                num-=1
+                print(num,dec2bin(num))
+                time.sleep(sleep_time)
     GPIO.output(leds,dec2bin(num))
